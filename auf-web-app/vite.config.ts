8f40e717913fa464488dfef435222f408ccb582c
reshikino <reshikino@gmail.com>
2024-06-15 19:12:52 +1000

    Добавление файлов index.css и main.tsx

--- auf-web-app/vite.config.ts
+++ auf-web-app/vite.config.ts
@@ -3,11 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // Ensure that assets are correctly resolved
   build: {
-    outDir: 'dist',
-  },
-  server: {
-    port: 5173,
-  },
-  base: '/',
+    rollupOptions: {
+      input: '/public/index.html' // Adjust this if necessary
+    }
+  }
 });
