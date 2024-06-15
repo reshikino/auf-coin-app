51e56777b8a6ae7980dfa4eb6b3f7ee6985ad8bf
reshikino <reshikino@gmail.com>
2024-06-15 22:11:40 +1000

    Fix paths and update configuration

--- auf-web-app/vite.config.ts
+++ auf-web-app/vite.config.ts
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
   build: {
     rollupOptions: {
-      input: 'index.html'
+      input: 'public/index.html'
     }
   }
 });
