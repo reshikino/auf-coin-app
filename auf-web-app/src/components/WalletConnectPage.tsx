172637c52de8ee2f203f036e62c6ea165694fcf9
reshikino <reshikino@gmail.com>
2024-06-15 20:55:14 +1000

    Initial commit with TON Connect integration

--- auf-web-app/src/components/WalletConnectPage.tsx
+++ auf-web-app/src/components/WalletConnectPage.tsx
@@ -12,9 +12,16 @@ const WalletConnectPage: React.FC = () => {
     }
   }, [wallet, navigate]);
 
+  useEffect(() => {
+    const tonConnectUI = new (window as any).TON_CONNECT_UI.TonConnectUI({
+      manifestUrl: 'https://raw.githubusercontent.com/reshikino/auf-coin-app/main/auf-web-app/public/tonconnect-manifest.json',
+      buttonRootId: 'ton-connect'
+    });
+  }, []);
+
   return (
-    <div id="root">
-      <TonConnectButton />
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <div id="ton-connect"></div>
     </div>
   );
 };
