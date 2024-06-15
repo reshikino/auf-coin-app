b31ae1624c71e0fcf26611892c7b46b474c4ab43
reshikino <reshikino@gmail.com>
2024-06-15 19:04:09 +1000

    Добавлены файлы index.css и main.tsx

--- auf-web-app/src/main.tsx
+++ auf-web-app/src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  return (
+    <div>
+      <h1>AUF Web App</h1>
+      <div id="ton-connect"></div>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
