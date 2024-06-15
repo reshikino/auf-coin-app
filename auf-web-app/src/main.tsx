b4ab9dcacb48d705b9a84857f8c147bdd7e04e0f
reshikino <reshikino@gmail.com>
2024-06-15 16:17:16 +1000

    Update manifest URL and fix integration

--- auf-web-app/src/main.tsx
+++ auf-web-app/src/main.tsx
@@ -3,7 +3,7 @@ import App from './App';
 import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
-const manifestUrl = 'https://raw.githubusercontent.com/reshikino/auf-coin-app/main/auf-web-app/tonconnect-manifest.json';
+const manifestUrl = 'https://raw.githubusercontent.com/reshikino/auf-coin-app/main/tonconnect-manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
