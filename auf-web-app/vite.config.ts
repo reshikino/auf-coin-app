72cc67faca02fe4fa3bd92d5295394926c8659bf
reshikino <reshikino@gmail.com>
2024-06-15 19:57:58 +1000

    Initial commit with TON Connect integration

--- auf-web-app/vite.config.ts
+++ auf-web-app/vite.config.ts
@@ -3,11 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: 'src', // Добавьте эту строку, чтобы указать, что корневая директория - это 'src'
   build: {
-    outDir: '../dist', // Убедитесь, что сборка происходит в папку 'dist'
     rollupOptions: {
-      input: 'public/index.html', // Убедитесь, что путь к 'index.html' указан правильно
+      input: 'public/index.html',
     },
   },
 });
