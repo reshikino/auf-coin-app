77899b474eb392026ac7e4e1ac499f2a1c598679
reshikino <reshikino@gmail.com>
2024-06-15 19:37:57 +1000

    Initial commit with TON Connect integration

--- auf-web-app/src/App.tsx
+++ auf-web-app/src/App.tsx
@@ -1,6 +1,6 @@
 // src/App.tsx
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import WalletConnectPage from './components/WalletConnectPage';
 import LanguageSelection from './components/LanguageSelection';
 import Terms from './components/Terms';
@@ -10,12 +10,12 @@ import './App.css';
 const App: React.FC = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={WalletConnectPage} />
-        <Route path="/language-selection" component={LanguageSelection} />
-        <Route path="/terms" component={Terms} />
-        <Route path="/loading" component={LoadingScreen} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<WalletConnectPage />} />
+        <Route path="/language-selection" element={<LanguageSelection />} />
+        <Route path="/terms" element={<Terms />} />
+        <Route path="/loading" element={<LoadingScreen />} />
+      </Routes>
     </Router>
   );
 };
