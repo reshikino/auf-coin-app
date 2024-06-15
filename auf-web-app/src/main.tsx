21dae4f6bf07dbd7e403d7c5c325226cb0efc957
reshikino <reshikino@gmail.com>
2024-06-15 20:59:35 +1000

    Initial commit with TON Connect integration

--- auf-web-app/src/main.tsx
+++ auf-web-app/src/main.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import WalletConnectPage from './components/WalletConnectPage';
+import LanguageSelection from './components/LanguageSelection';
+import Terms from './components/Terms';
+import LoadingScreen from './components/LoadingScreen';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<WalletConnectPage />} />
+        <Route path="/language" element={<LanguageSelection />} />
+        <Route path="/terms" element={<Terms />} />
+        <Route path="/loading" element={<LoadingScreen />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
