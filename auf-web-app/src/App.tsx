68c308facfd5263fa237de6e9c6b24b0cf87f840
reshikino <reshikino@gmail.com>
2024-06-15 15:30:36 +1000

    Add auf-web-app files to main repository

---
+++ auf-web-app/src/App.tsx
@@ -0,0 +1,15 @@
+import { TonConnectButton } from '@tonconnect/ui-react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>AUF Web App</h1>
+        <TonConnectButton />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ auf-web-app/src/main.tsx
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
+
+const manifestUrl = 'https://raw.githubusercontent.com/reshikino/auf-coin-app/main/auf-web-app/tonconnect-manifest.json';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <TonConnectUIProvider manifestUrl={manifestUrl}>
+    <App />
+  </TonConnectUIProvider>,
+);
