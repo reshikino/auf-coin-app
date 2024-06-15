fb69058063df11d3501c02aa45bfccaed8fa2e52
reshikino <reshikino@gmail.com>
2024-06-15 22:42:26 +1000

    Setup project with routing and TonConnect

--- auf-web-app/vite.config.ts
+++ auf-web-app/vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensure correct base path
+  base: './',
+  build: {
+    rollupOptions: {
+      input: './public/index.html'
+    }
+  }
 });
